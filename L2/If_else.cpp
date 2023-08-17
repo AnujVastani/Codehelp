03e42a4cd680719a9f169599932977a9fdecfccb
Anuj Vastani <130305405+AnujVastani@users.noreply.github.com>
2023-08-18 04:49:35 +0530

    Program to check whether the number is postive or negative
    In this code we are using if else loop to check a number is positive or negative. If the number is greater than 0 than it is positive and t is less than zero than it  is negative.

---
+++ L2/If_else.cpp
@@ -0,0 +1,16 @@
+//Program to check whether the number is posotive or negative
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+    if(n>0){
+        cout<<"Number is positive"<<endl;
+    }
+    else{
+        cout<<"Number is negative";
+    }
+    return 0;
+    }
